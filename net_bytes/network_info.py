6c25d5596badb89b36101ab2358ceddd5229fc9a
Takeru Harashima <takerujoker936@gmail.com>
2025-01-07 22:16:01 +0900

    Change a net_bytes

--- net_bytes/network_info.py
+++ net_bytes/network_info.py
@@ -6,25 +6,19 @@ from rclpy.node import Node
 from std_msgs.msg import String
 import psutil
 
-class NetworkInfoTalker(Node):
-    def __init__(self):
-        super().__init__('network_info')
-        self.pub = self.create_publisher(String, 'network_info', 10)
-        self.timer = self.create_timer(1.0, self.timer_callback)
+rclpy.init()
+node = Node("network_info")
+pub = node.create_publisher(String, "network_info", 10)
 
-    def timer_callback(self):
-        net_io = psutil.net_io_counters()
-        bytes_sent = net_io.bytes_sent / (1024 ** 2)
-        bytes_recv = net_io.bytes_recv / (1024 ** 2)
-
-        # ネットワーク情報をフォーマット
-        net_bytes = f"送信バイト: {bytes_sent:.2f}MB, 受信バイト: {bytes_recv:.2f}MB"
-
-        msg = String()
-        msg.data = net_bytes
-        self.pub.publish(msg)
+def cb():
+    msg = String()
+    net_io = psutil.net_io_counters()
+    bytes_sent = net_io.bytes_sent / (1024 ** 2)
+    bytes_recv = net_io.bytes_recv / (1024 ** 2)
+    net_bytes = f"送信バイト: {bytes_sent:.2f}MB, 受信バイト: {bytes_recv:.2f}MB"
+    msg.data = net_bytes
+    pub.publish(msg)
 
 def main():
-    rclpy.init()
-    network_info_talker = NetworkInfoTalker()
-    rclpy.spin(network_info_talker)
+    node.create_timer(1.0, cb)  # 1秒ごとにcallbackを実行
+    rclpy.spin(node)
